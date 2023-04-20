4d9feb3943e3c87f260f5ea818e86f0e8f8497d3
ilia-io <likeapu@gmail.com>
2023-04-20 19:18:29 +0300

    add redux

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Counter from '@/store/slices/Counter/Counter';
 
 type Props = {};
 
 const about = (props: Props) => {
   return (
     <div>
-      about
+      <Counter />
       <Stack spacing={2} direction="row">
         <Button variant="text">Text</Button>
         <Button variant="contained">Contained</Button>
