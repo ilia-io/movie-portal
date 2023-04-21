073933f96799b69ab85d0a69eaaa4e73c273db0a
ilia-io <likeapu@gmail.com>
2023-04-21 19:30:55 +0300

    feat: add layout component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import store from '@/store/store';
 import '@/styles/globals.css';
+import Layout from '@/widgets/Layout';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <Layout>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </Layout>
   );
 }
